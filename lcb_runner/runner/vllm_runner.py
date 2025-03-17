d0822b57401faeedc997f61eedddc6f0d2153df8
blbadger <blbadger10@gmail.com>
2025-03-17 20:32:13 +0000

    removed prefix chunk for v100

--- lcb_runner/runner/vllm_runner.py
+++ lcb_runner/runner/vllm_runner.py
@@ -23,6 +23,8 @@ class VLLMRunner(BaseRunner):
             disable_custom_all_reduce=True,
             enable_prefix_caching=args.enable_prefix_caching,
             trust_remote_code=args.trust_remote_code,
+	    enable_chunked_prefill=False,
+		
         )
         self.sampling_params = SamplingParams(
             n=self.args.n,
