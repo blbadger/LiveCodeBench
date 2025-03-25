97d67a6877365bc2e3e165c733c90498ef8808d9
blbadger <blbadger10@gmail.com>
2025-03-25 21:53:21 +0000

    gen edits

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 python -m lcb_runner.runner.main \
---model meta-llama/Meta-Llama-3.1-8B-Instruct \
---local_model_path "/home/bbadger/llama-3.1-8b-codeforcescots-qlora-b128/merged_model" \
+--model "meta-llama/Meta-Llama-3.1-8B" \
+--local_model_path "/home/bbadger/Desktop/deepseek-llama-3.1-8b" \
 --scenario codegeneration \
 --cot_code_execution \
 --evaluate \
 --dtype float16 \
---max_tokens 8192 \
+--max_tokens 4096 \
 --tensor_parallel_size 4 \
 --n 1 \
---release_version release_v1 \
---temperature 0.6 \
+--release_version release_v5 \
+--temperature 0.58 \
 --top_p 0.95
 #--use_cache
 
